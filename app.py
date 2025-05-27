d59f1ede098ef47bd25c0d3c89b8b39aac94bfc9
kammouti <kammmouti@gmail.com>
2025-05-27 17:05:29 +0100

    login system

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 
 
 load_dotenv()
@@ -22,6 +23,16 @@ db = SQLAlchemy(app)
 # migration instance
 migrate = Migrate(app,db)
 
+# Login:
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def user_loader(user_id):
+    return User.query.get(int(user_id))
+
+
 # creating Models
 class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -33,8 +44,7 @@ class Article(db.Model):
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
 
-
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     full_name = db.Column(db.String(300))
     username = db.Column(db.String(200), nullable=False, unique=True)
@@ -58,6 +68,7 @@ class User(db.Model):
 # create an article form class
 class ArticleFrom(FlaskForm):
     title = StringField("Titre de l'article", validators=[DataRequired()])
+    slug = StringField("Slug de l'article", validators=[DataRequired()])
     extrait = TextAreaField("Extrait de l'article", validators=[DataRequired()])
     content = TextAreaField("Contenu de l'article", validators=[DataRequired()])
     category = SelectField("Categorie",validators=[DataRequired()],choices=["General", "Lois de conduite", "Lois au Maroc", "Technique de conduite", "Evenement"])
@@ -86,6 +97,7 @@ def index():
     return render_template("index.html")
 
 @app.route("/add_user", methods=['GET', 'POST'])
+@login_required
 def add_user():
     form = UserFrom()
     # user_list = db.session.query(User).order_by(User.created_at).all()
@@ -115,6 +127,7 @@ def add_user():
 
 
 @app.route("/delete_user/<int:id>")
+@login_required
 def delete_user(id):
     form = UserFrom()
     user_list = User.query.order_by(User.created_at)
@@ -122,7 +135,7 @@ def delete_user(id):
     try:
         db.session.delete(user_to_delete)
         db.session.commit()
-        flash("Utilisateur suprime avec succes")
+        flash("Utilisateur suprime avec succes",)
         form.username.data = ''
         form.full_name.data = ''
         form.password_hash.data = ''
@@ -136,6 +149,7 @@ def delete_user(id):
 
 
 @app.route("/add_article", methods=['GET', 'POST'])
+@login_required
 def add_article():
     form = ArticleFrom()
     if form.validate_on_submit():
@@ -144,60 +158,61 @@ def add_article():
             # add data to database
             article = Article(
             title = form.title.data,
+            slug = form.slug.data,
             content = form.content.data,
             extrait = form.extrait.data,
             category = form.category.data
             )
             db.session.add(article)
             db.session.commit()
-            flash(f'Article - {form.title.data} - ajoutee avec succee!')
+            flash(f'Article - {form.title.data} - ajoutee avec succee!', 'success')
             return redirect(url_for('articles'))
         else:
-            flash("Titre de l'article exist deja! changer le titre ou verifier la liste des articles")
+            flash("Titre de l'article exist deja! changer le titre ou verifier la liste des articles", 'error')
     return render_template("add_article.html", form=form)
 
 # edit article witrh existinfg title
 
 @app.route("/edit_article/<int:id>", methods=['GET', 'POST'])
+@login_required
 def edit_article(id):
     form = ArticleFrom()
     article_to_edit = Article.query.get_or_404(id)
-    exist = Article.query.filter_by(title=request.form.get('title')).first()
-    if exist is None:
-        if request.method == "POST":
-            article_to_edit.title = request.form.get('title')
-            article_to_edit.content = request.form.get('content')
-            article_to_edit.extrait = request.form.get('extrait')
-            article_to_edit.category = request.form.get('category')
+    duplicate = Article.query.filter(Article.id != article_to_edit.id, Article.title==request.form.get('title'),).first()
+    if request.method == "POST":
+        article_to_edit.title = request.form.get('title')
+        article_to_edit.slug = request.form.get('slug')
+        article_to_edit.content = request.form.get('content')
+        article_to_edit.extrait = request.form.get('extrait')
+        article_to_edit.category = request.form.get('category')
+        
+        if duplicate:
+            flash("Titre de l'article deja existe, essayez a nouveau!", 'message')
+            return redirect(url_for('edit_article', id =id))
+        else:
             try:
                 db.session.commit()
-                flash(f"Article - {article_to_edit.title} - modifiee avec succee")
+                flash(f"Article - {article_to_edit.title} - modifiee avec succee", "success")
                 return redirect(url_for('articles'))
             except:
-                flash("Oups! un probleme est survenu, esseyer a nouveau!")
-                return redirect(url_for('edit_article/<int:id>', id =id))
-        else:
-            return render_template("edit_article.html", form=form, article_to_edit=article_to_edit)
-    else:
-        # Make sure the title doesnt exist:
-        flash("Titre de l'article deja existe, essayez a nouveau!")
-        return render_template("edit_article.html", form=form, article_to_edit=article_to_edit)
-    
-
+                flash("Oups! un probleme est survenu, esseyer a nouveau!", "error")
+                return redirect(url_for('edit_article', id =id))
+    return render_template("edit_article.html", form=form, article_to_edit=article_to_edit)
 
 
 @app.route("/delete_article/<int:id>")
+@login_required
 def delete_article(id):
     article_to_delete = Article.query.get_or_404(id)
     try:
         db.session.delete(article_to_delete)
         db.session.commit()
-        flash("Article suprime avec succes.")
+        flash("Article suprime avec succes.", "success")
         # return render_template("articles.html",
         #                        articles=articles)
         return redirect(url_for('articles'))
     except:
-        flash("Oups! Esseyez a nouveau!")
+        flash("Oups! Esseyez a nouveau!", "error")
         # return render_template("articles.html",
         #                        articles=articles)
         return redirect(url_for('articles'))
@@ -209,32 +224,47 @@ def articles():
     articles = db.session.query(Article).order_by(Article.created_at).all()
     return render_template("articles.html", articles=articles)
 
+@app.route("/articles/<int:id>")
+def article(id):
+    article = db.session.query(Article).get_or_404(id)
+    return render_template("article.html", article=article)
 
-@app.route("/admin/<username>")
-def admin(username):
+
+@app.route("/admin_dash/<username>")
+def admin_dash(username):
     return render_template("admin.html", username=username)
 
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
     form = LoginFrom()
     if form.validate_on_submit():
         username = form.username.data
-        password = form.password_hash.data
+        password_hash = form.password_hash.data
         
         user_to_check = User.query.filter_by(username=username).first()
         if user_to_check:
-            if user_to_check.verify_password(password):
-                flash('User logged-in avec succee!')
+            if user_to_check.verify_password(password_hash):
+                login_user(user_to_check)
+                flash('Utilisateur connecte avec succee!', "success")
                 return redirect(url_for('articles'))
             else:
-                flash('Mot de passe incorect!')
+                flash('Mot de passe incorect!', "error")
         else:
-            flash("Nom d'utilisateur incorrect")
+            flash("Nom d'utilisateur n'exist pas!", "message")
         
     return render_template("login.html", form = form)
 
 
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 # Costum error pages:
 
